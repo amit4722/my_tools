2626ce4e612bf4770e7114aab1c901642ebdba75
amit4722 <amit4722@DESKTOP-O7RQ7K9>
2017-06-19 21:36:16 +0530

    Commit changes

---
+++ git_example/src/git_example/org/DisplayGit.java
@@ -0,0 +1,14 @@
+package git_example.org;
+
+public class DisplayGit {
+	
+	
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Inside Display Git");
+		
+	}
+	
+
+
+}
---
+++ git_example/src/git_example/org/HelloGit.java
@@ -0,0 +1,15 @@
+package git_example.org;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		
+		DisplayGit displayGit=new DisplayGit();
+		
+		System.out.println("Inside Hello Git");
+		displayGit.display();
+		
+
+	}
+
+}
